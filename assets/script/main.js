0098037746f3fc57ee49c31e6d0daa0fd9fcb2d4
jtoufik <112856590+jtoufik@users.noreply.github.com>
2022-12-12 11:50:40 +0100

    rotate
    mijn eerste interactie!

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,15 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+
+//interactie 1 //
+
+let frontend = document.querySelector('a:nth-of-type(1)')
+
+frontend.addEventListener('click', rotateHandler)
+frontend.addEventListener('animationend', rotateHandler)
+
+function rotateHandler(){
+  frontend.classList.toggle('rotate')
+}
